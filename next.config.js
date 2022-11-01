70792f053787e336f01a3e868231171da5709466
George clark <george_clark@georges-mbp.home>
2022-11-01 17:18:07 +0000

    fix 404

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   experimental: {
     runtime: "experimental-edge",
   },
+  reactStrictMode: true,
+  swcMinify: true,
 };
-
 module.exports = nextConfig;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,10 @@
-export const config = {
-  runtime: "experimental-edge",
-};
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 
 export default async function (req) {
-  return new Response(JSON.stringify({ name: "John Doe" }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  return new Response(
+    JSON.stringify({
+      name: "John Doe",
+    }),
+    { status: 200, headers: { "Content-Type": "application/json" } }
+  );
 }
